766e37184355f10a6f7c4ec1e7ba9de23f93548a
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-06-13 22:13:26 +0700

    First update, color palette adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
     extend: {
       screens: {
         '2xl': '1320px',
-      }
+      },
+      colors: {
+        PW: '#ffffff',
+        PY: '#d1ae23',
+        PBM: '#436387',
+        PBT: '#0a3757',
+        PB: '#000000',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
