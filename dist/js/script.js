5dcda1604bb65351ff835baea05516fd5e4b0401
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-07-20 12:25:15 +0700

    update transition full section

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,3 +20,28 @@ humberger.addEventListener('click', function() {
     humberger.classList.toggle('humberger-active');
     navMenu.classList.toggle('hidden');
 }); 
+
+//Transisi
+document.addEventListener('DOMContentLoaded', (event) => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if (entry.target.classList.contains('primary-fade-in-hidden')) {
+            entry.target.classList.remove('primary-fade-in-hidden');
+            entry.target.classList.add('primary-fade-in');
+            } else if (entry.target.classList.contains('secondary-fade-in-hidden')) {
+            entry.target.classList.remove('secondary-fade-in-hidden');
+            entry.target.classList.add('secondary-fade-in');
+            }
+        }
+        });
+    });
+    
+    document.querySelectorAll('.primary-fade-in-hidden').forEach(el => {
+        observer.observe(el);
+    });
+    
+    document.querySelectorAll('.secondary-fade-in-hidden').forEach(el => {
+        observer.observe(el);
+    });
+});
